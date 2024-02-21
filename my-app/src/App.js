30558715b502ea378f3bab11a9642795aa01352b
Mark Nolan <marknolan@Marks-MacBook-Air.local>
2024-02-21 18:06:04 +0000

    Added React Project

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>
+        {" "}
+        <a href="/Catcher/index.html">Hello</a>{" "}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
