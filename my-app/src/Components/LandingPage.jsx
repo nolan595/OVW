cbae0c768c1ed177856a2a3967917e2e90c5790a
Mark Nolan <marknolan@Marks-MacBook-Air.local>
2024-02-22 14:20:48 +0000

    Added components folder

---
+++ my-app/src/Components/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      {" "}
+      <iframe
+        src="/Catcher/index.html"
+        width="800"
+        height="600"
+        title="Godot Game"
+      ></iframe>
+    </div>
+  );
+}
+
+export default LandingPage;
