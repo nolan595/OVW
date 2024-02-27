be49ab0b499603dc775ab867d8a555ed7371a544
Mark Nolan <marknolan@Marks-MacBook-Air.local>
2024-02-27 21:55:16 +0000

    removed hide game button

--- my-app/src/Components/LandingPage.jsx
+++ my-app/src/Components/LandingPage.jsx
@@ -26,16 +26,15 @@ function LandingPage() {
   return (
     <div className="landingPage">
       <div className="mainContent">
-        <button onClick={() => setShowIframe(!showIframe)}>
-          {showIframe ? "Hide Game" : "Play Game"}
-        </button>
+        {/* Button now only shows "Play Game" and sets showIframe to true */}
+        {!showIframe && (
+          <button onClick={() => setShowIframe(true)}>Play Game</button>
+        )}
       </div>
       {showIframe && (
         <div className="iframeContainer">
           <iframe src="/Catcher/index.html" title="Game"></iframe>
-          <button className="closeButton" onClick={() => setShowIframe(false)}>
-            X
-          </button>
+          {/* Removed the toggle button functionality, so no need for a close button here */}
         </div>
       )}
     </div>
