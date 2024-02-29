0dbe46163202e912fae366bb015fca5ef92fbd5f
Mark Nolan <marknolan@Marks-MacBook-Air.local>
2024-02-29 23:40:41 +0000

    testing static files

--- my-app/src/Components/GameOver.jsx
+++ my-app/src/Components/GameOver.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import "../styles/gameOver.css";
-import copy from "copy-to-clipboard";
-import { useNavigate, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 function GameOver() {
-  const gameUrl = "https://ovw-3pigs.netlify.app/";
-  const location = useLocation();
-  const score = location.state?.score || "500"; // default score to 500
-
-  const handleShareClick = () => {
-    copy(gameUrl);
-    alert("URL copied to clipboard!");
-  };
+  // const gameUrl = "https://ovw-3pigs.netlify.app/";
 
   return (
     <motion.div
