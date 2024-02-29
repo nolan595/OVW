96c40724117d1afd001bc8a4bef4d63f24c4f7f9
Mark Nolan <marknolan@Marks-MacBook-Air.local>
2024-02-29 14:36:16 +0000

    Social sharing

--- my-app/src/Components/GameOver.jsx
+++ my-app/src/Components/GameOver.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles/gameOver.css";
 import copy from "copy-to-clipboard";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FiShare2 } from "react-icons/fi"; // Importing a share icon
 
 function GameOver() {
   const gameUrl = "https://ovw-3pigs.netlify.app/";
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const location = useLocation();
   const score = location.state?.score || "0";
   // const shareText = `I scored ${score} in the game! Can you beat me? Check out this awesome game: ${gameUrl}`;
