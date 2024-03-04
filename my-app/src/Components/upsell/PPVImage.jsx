eed61c1209633c0d662b75bf1fbb864a084150c6
Mark Nolan <marknolan@marks-air.home>
2024-03-04 00:03:58 +0000

    Updated character selection

--- my-app/src/Components/upsell/PPVImage.jsx
+++ my-app/src/Components/upsell/PPVImage.jsx
@@ -3,10 +3,11 @@ import styles from "../../styles/components/ppvImage.module.css";
 import ppvImage from "../../images/upsell.jpeg";
 function PPVImage() {
   return (
-    <div>
+    <div className={styles.ppvContainer}>
       <img className={styles.ppvImage} src={ppvImage} alt="" />
       <div className={styles.ppvText}>Will the Aliens take over OVW?</div>
       <div className={styles.ppvText}>Find out on the 16th of March</div>
+      <button className={styles.ppvButton}>PPV Info</button>
     </div>
   );
 }
