a0b6e3529b565df566b71cc599eb9cb6cbf18032
Mark Nolan <marknolan@Marks-MacBook-Air.local>
2024-03-05 15:48:37 +0000

    Google A

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AnimtedRoutes } from "./Components"; // Import your GameOverPage component
+import ReactGA from "react-ga";
+
+ReactGA.initialize("G-E52JG68JHK");
 
 function App() {
   return (
