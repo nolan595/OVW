4f20ecc9a2490ac89e1b34c2f8ca705063e83b5d
Mark Nolan <marknolan@Marks-MacBook-Air.local>
2024-04-10 15:57:52 +0100

    load testing

---
+++ script.js
@@ -0,0 +1,15 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "2m", target: 100 }, // 100 users over 2 mins
+    { duration: "5m", target: 100 }, // stay at 100 users for 5
+    { duration: "2m", target: 0 }, // slowly back down
+  ],
+};
+
+export default function () {
+  http.get("https://ovw-3pigs.netlify.app/game-over");
+  sleep(1);
+}
